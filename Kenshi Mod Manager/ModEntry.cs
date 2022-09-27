163fa7e55a26ae67b08ecc7ecd3e7b070f28a0df
Kevin Serra <kevindanielserra@gmail.com>
2022-09-27 10:46:48 -0400

    Create ModEntry.cs

---
+++ Kenshi Mod Manager/ModEntry.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace Kenshi_Mod_Manager
+{
+    public class ModEntry
+    {
+        public readonly string Name;
+        public readonly string FileName;
+        public readonly Image Image;
+        public readonly string[] Categories;
+        public readonly ModSource Source;
+
+        public ModEntry(
+            string name,
+            string fileName,
+            Image image,
+            string[] categories,
+            ModSource source)
+        {
+            Name = name;
+            FileName = fileName;
+            Image = image;
+            Categories = categories;
+            Source = source;
+        }
+    }
+
+}
