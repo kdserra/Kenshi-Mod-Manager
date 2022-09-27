f9cf0f9f1b8f624cb6d692d62da5b095876135a0
Kevin Serra <kevindanielserra@gmail.com>
2022-09-27 10:46:50 -0400

    Update ModManagerForm.cs

--- Kenshi Mod Manager/ModManagerForm.cs
+++ Kenshi Mod Manager/ModManagerForm.cs
@@ -1,14 +1,47 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace Kenshi_Mod_Manager
 {
     public partial class ModManagerForm : Form
     {
+        private List<ModEntry> m_ModEntryList = new List<ModEntry>() { };
+
         public ModManagerForm()
         {
             InitializeComponent();
             this.Select();
             this.Focus();
+            HideReplicaModTableEntry();
+        }
+
+        private void ClearModTable(ModEntry modEntry)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool AddToModTable(ModEntry modEntry)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool RemoveFromModTable(ModEntry modEntry)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SyncModTable(ModEntry modEntry)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HideReplicaModTableEntry()
+        {
+            replicaModInfoTableLayoutPanel.Visible = false;
+            replicaModCategoryLabel.Visible = false;
+            replicaModSourceLabel.Visible = false;
+            replicaModStateButton.Visible = false;
         }
     }
 }
