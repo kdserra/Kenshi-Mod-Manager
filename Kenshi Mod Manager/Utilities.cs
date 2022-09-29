c6a6403b43055aeacaa7166e2e3061a6c886d99c
Kevin Serra <kevindanielserra@gmail.com>
2022-09-29 18:17:53 -0400

    Create Utilities.cs

---
+++ Kenshi Mod Manager/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Kenshi_Mod_Manager
+{
+    public static class Utilities
+    {
+        public async static Task<Image> GetImage(string url)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                using (Stream stream = webClient.OpenRead(url))
+                {
+                    return Image.FromStream(stream);
+                }
+            }
+        }
+    }
+}
