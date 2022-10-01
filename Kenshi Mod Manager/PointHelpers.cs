2c99778184e45b0e64be941e4e23c898e53fff03
Kevin Serra <kevindanielserra@gmail.com>
2022-09-30 20:42:43 -0400

    Create PointHelpers.cs

---
+++ Kenshi Mod Manager/PointHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace Kenshi_Mod_Manager
+{
+    public static class PointHelpers
+    {
+        public static Point WithX(this Point point, int x)
+        {
+            return new Point(x, point.Y);
+        }
+
+        public static Point WithY(this Point point, int y)
+        {
+            return new Point(point.X, y);
+        }
+    }
+}
