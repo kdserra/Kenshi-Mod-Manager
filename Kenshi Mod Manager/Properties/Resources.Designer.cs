ff6ed4c193e54cfdf7c80f2e6de96c9b6df210e8
Kevin Serra <kevindanielserra@gmail.com>
2022-10-14 22:58:20 -0400

    Update Resources.Designer.cs

--- Kenshi Mod Manager/Properties/Resources.Designer.cs
+++ Kenshi Mod Manager/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Kenshi_Mod_Manager.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap black_box {
+            get {
+                object obj = ResourceManager.GetObject("black_box", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
